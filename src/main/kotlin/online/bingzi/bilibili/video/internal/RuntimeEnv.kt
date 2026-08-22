2cc5a4f361d532336585a4d0210b40cecda70255
BalanceSea <3643203568@qq.com>
2026-08-23 00:49:39 +0800

    Revert "fix(runtime): 确保 Kotlin 反射依赖随运行环境加载"
    This reverts commit 0685293cd310186cb0b5ad7fe20ba334666578e7.

--- src/main/kotlin/online/bingzi/bilibili/video/internal/RuntimeEnv.kt
+++ src/main/kotlin/online/bingzi/bilibili/video/internal/RuntimeEnv.kt
@@ -15,10 +15,7 @@ import taboolib.common.env.RuntimeDependency
 @RuntimeDependencies(
     RuntimeDependency(
         value = "!org.jetbrains.kotlin:kotlin-reflect:2.2.0",
-        /**
-         * 部分用户出现过了加载测试却在最终没有加载该库
-         */
-        //test = "!kotlin.reflect.full.KClasses",
+        test = "!kotlin.reflect.full.KClasses",
         transitive = false
     ),
     RuntimeDependency(
